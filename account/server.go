689080721793b3c278d3f15538d199c95d1462a4
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2025-05-15 00:13:35 +0330

    listen grpc server

--- account/server.go
+++ account/server.go
@@ -2,10 +2,26 @@ package account
 
 import (
 	"context"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type grpcServer struct {
 	service Service
+}
+
+func ListenGRPC(s Service, port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+	serv := grpc.NewServer()
+	pb.RegisterAccountServiceServer(serv, &grpcServer{s})
+	reflection.Register(serv)
+	return serv.Serve(lis)
 }
 
 func (s *grpcServer) PostAccount(ctx context.Context, r *pb.PostAccountRequest) (*pb.PostAccountResponse, error) {
