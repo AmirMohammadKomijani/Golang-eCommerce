6a3d377d7a258a61a9251f7bbcc14012bcd4465e
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2025-05-15 00:10:59 +0330

    post-account server

--- account/server.go
+++ account/server.go
@@ -1,5 +1,20 @@
 package account
 
+import (
+	"context"
+)
+
 type grpcServer struct {
 	service Service
+}
+
+func (s *grpcServer) PostAccount(ctx context.Context, r *pb.PostAccountRequest) (*pb.PostAccountResponse, error) {
+	a, err := s.service.PostAccount(ctx, r.Name)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.PostAccountResponse{Account: &pb.Account{
+		Id:   a.ID,
+		Name: a.Name,
+	}}, nil
 }
