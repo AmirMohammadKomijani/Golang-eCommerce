362713f8dd18340ffbbe162e68107d6186e4d179
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2025-05-18 16:46:35 +0330

    all get methods for products are now implemented

--- catalog/service.go
+++ catalog/service.go
@@ -41,3 +41,18 @@ func (s *catalogService) PostProduct(ctx context.Context, name, description stri
 	}
 	return p, nil
 }
+
+func (s *catalogService) GetProduct(ctx context.Context, id string) (*Product, error) {
+	return s.repository.GetProductByID(ctx, id)
+}
+
+func (s *catalogService) GetProducts(ctx context.Context, skip uint64, take uint64) ([]Product, error) {
+	if take > 100 || (skip == 0 && take == 0) {
+		take = 100
+	}
+	return s.repository.ListProducts(ctx, skip, take)
+}
+
+func (s *catalogService) GetProductsByIDs(ctx context.Context, ids []string) ([]Product, error) {
+	return s.repository.ListProductsWithIDs(ctx, ids)
+}
