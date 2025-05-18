0ec939467521a39085394fbdcd837c5cf10498c5
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2025-05-18 17:26:52 +0330

    client.go is implemented

---
+++ catalog/client.go
@@ -0,0 +1,21 @@
+package catalog
+
+import "google.golang.org/grpc"
+
+type Client struct {
+	conn    *grpc.ClientConn
+	service pb.CatalogServiceClient
+}
+
+func NewClient(url string) (*Client, error) {
+	conn, err := grpc.Dial(url, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	c := pb.NewCatalogServiceClient(conn)
+	return &Client{conn, c}, nil
+}
+
+func (c *Client) Close() {
+	c.conn.Close()
+}
