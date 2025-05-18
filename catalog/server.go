3b8d8d0d617cad289d88e25fca84f533a30e00fc
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2025-05-18 16:53:09 +0330

    server.go is now ready

---
+++ catalog/server.go
@@ -0,0 +1,24 @@
+package catalog
+
+import (
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type grpcServer struct {
+	service Service
+}
+
+func ListenGRPC(s Service, port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+	serv := grpc.NewServer()
+	pb.RegisterCatalogServiceServer(serv, &grpcServer{s})
+	reflection.Register(serv)
+	return serv.Serve(lis)
+}
