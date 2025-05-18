b3901b2702def4da184b4bd617d5340b87099cab
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2025-05-18 16:53:59 +0330

    get product in server.go file

--- catalog/server.go
+++ catalog/server.go
@@ -38,3 +38,49 @@ func (s *grpcServer) PostProduct(ctx context.Context, r *pb.PostProductRequest) 
 		Price:       p.Price,
 	}}, nil
 }
+
+func (s *grpcServer) GetProduct(ctx context.Context, r *pb.GetProductRequest) (*pb.GetProductResponse, error) {
+	p, err := s.service.GetProduct(ctx, r.Id)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return &pb.GetProductResponse{
+		Product: &pb.Product{
+			Id:          p.ID,
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+		},
+	}, nil
+}
+
+func (s *grpcServer) GetProducts(ctx context.Context, r *pb.GetProductsRequest) (*pb.GetProductsResponse, error) {
+	var res []Product
+	var err error
+	if r.Query != "" {
+		res, err = s.service.SearchProducts(ctx, r.Query, r.Skip, r.Take)
+	} else if len(r.Ids) != 0 {
+		res, err = s.service.GetProductsByIDs(ctx, r.Ids)
+	} else {
+		res, err = s.service.GetProducts(ctx, r.Skip, r.Take)
+	}
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	products := []*pb.Product{}
+	for _, p := range res {
+		products = append(
+			products,
+			&pb.Product{
+				Id:          p.ID,
+				Name:        p.Name,
+				Description: p.Description,
+				Price:       p.Price,
+			},
+		)
+	}
+	return &pb.GetProductsResponse{Products: products}, nil
+}
