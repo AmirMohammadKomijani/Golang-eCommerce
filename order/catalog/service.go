4fcd62bc1f877a82fe0ad7a1d186789e50d31143
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2025-05-19 20:45:39 +0330

    service-order functions are ready!

--- order/catalog/service.go
+++ order/catalog/service.go
@@ -3,6 +3,8 @@ package order
 import (
 	"context"
 	"time"
+
+	"github.com/segmentio/ksuid"
 )
 
 type Service interface {
@@ -32,4 +34,31 @@ type orderService struct {
 
 func NewService(r Repository) Service {
 	return &orderService{r}
+}
+
+func (s orderService) PostOrder(
+	ctx context.Context,
+	accountID string,
+	products []OrderedProduct,
+) (*Order, error) {
+	o := &Order{
+		ID:        ksuid.New().String(),
+		CreatedAt: time.Now().UTC(),
+		AccountID: accountID,
+		Products:  products,
+	}
+	// Calculate total price
+	o.TotalPrice = 0.0
+	for _, p := range products {
+		o.TotalPrice += p.Price * float64(p.Quantity)
+	}
+	err := s.repository.PutOrder(ctx, *o)
+	if err != nil {
+		return nil, err
+	}
+	return o, nil
+}
+
+func (s orderService) GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error) {
+	return s.repository.GetOrdersForAccount(ctx, accountID)
 }
