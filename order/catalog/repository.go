ab6cae4e3d252ac1937ae99932879b5edb77750d
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2025-05-19 20:32:30 +0330

    repository for postgres

--- order/catalog/repository.go
+++ order/catalog/repository.go
@@ -14,3 +14,19 @@ type Repository interface {
 type postgresRepository struct {
 	db *sql.DB
 }
+
+func NewPostgresRepository(url string) (Repository, error) {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return &postgresRepository{db}, nil
+}
+
+func (r *postgresRepository) Close() {
+	r.db.Close()
+}
