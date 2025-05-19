359dda101000dea04877cf1f824f14844da87d9f
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2025-05-19 22:37:25 +0330

    queryresolver

--- graphql/graph.go
+++ graphql/graph.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/AmirMohammadKomijani/Golang-eCommerce/account"
+	"github.com/AmirMohammadKomijani/Golang-eCommerce/catalog"
+	"github.com/AmirMohammadKomijani/Golang-eCommerce/order"
+)
+
 type Server struct {
 	accountClient *account.Client
 	catalogClient *catalog.Client
@@ -33,4 +40,28 @@ func NewGraphQLServer(accountUrl, catalogURL, orderURL string) (*Server, error) 
 		catalogClient,
 		orderClient,
 	}, nil
+}
+
+func (s *Server) Mutation() MutationResolver {
+	return &mutationResolver{
+		server: s,
+	}
+}
+
+func (s *Server) Query() QueryResolver {
+	return &queryResolver{
+		server: s,
+	}
+}
+
+func (s *Server) Account() AccountResolver {
+	return &accountResolver{
+		server: s,
+	}
+}
+
+func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
+	return NewExecutableSchema(Config{
+		Resolvers: s,
+	})
 }
